eb3d8f57627f5c7d69c9ea8960cfc5d124f07cb5
Ben McCormick <ben.mccormick@alumni.duke.edu>
2013-12-17 16:00:24 -0500

    Initial commit

---
+++ templates/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php wp_title('|', true, 'right'); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php wp_head(); ?>
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,300italic' rel='stylesheet' type='text/css'>
+  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+</head>
