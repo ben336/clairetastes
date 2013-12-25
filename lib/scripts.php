14a0fc0564de869f74e7af61d13b4bf434e64b83
Ben McCormick <ben.mccormick@alumni.duke.edu>
2013-12-25 16:57:02 -0500

    Added some font fallbacks

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a9b8c7bd75308f696704af8ddbc5fc70');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bf03eddff2cd1cb4ee6c7b1ba56ebefc');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
